a62cc4f28c6786cb7edf8fb180fc80b16f645146
Ryan Simmons <ryansimmons1428@gmail.com>
2025-09-17 10:12:23 -0300

    Event Class created

---
+++ TennisSource/Models/event.cs
@@ -0,0 +1,12 @@
+﻿namespace TennisSource.Models
+{
+    public class @event
+    {
+        public int EventId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Location {  get; set; } = string.Empty;
+        public string Date { get; set; } = string.Empty;
+
+    }
+}
